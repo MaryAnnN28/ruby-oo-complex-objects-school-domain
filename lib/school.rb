96693a6f5b706ecc7736dad9f269b5b0ffeb74c7
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-11-19 22:30:32 -0500

    done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster, :name
+    
+    def initialize(name)
+        @roster = {}
+        @name = name 
+    end
+
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade_level)
+        @roster[grade_level]
+    end
+
+
+    def sort
+        roster.each_value { |student_list| student_list.sort!}  
+        # using "sort!"" sorts the names within each array by grade
+    end
+        
+end
